9588c07dca78543b62f27570b2b066a561053215
JH-devs <jitka.horavova01@gmail.com>
2024-01-09 19:05:13 +0100

    commit

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,5 +1,6 @@
 import Container from "@/components/ui/container";
 import Link from "next/link";
+import MainNav from "@/components/main-nav";
 
 const Navbar = () => {
     return (
@@ -9,7 +10,7 @@ const Navbar = () => {
                     <Link href="/" className="ml-4 flex lg:ml-0 gap-x-2">
                         <p className="font-bold text-xl store-title">eshop-obchod</p>
                     </Link>
-                    <MainNAv />
+                    <MainNav data={[]} />
                 </div>
             </Container>
         </div>
